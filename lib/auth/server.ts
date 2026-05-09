0f0ba99bcf095d579fee3398f58554f7c38c2989
Gianluca Bessone <bessone.gianluca@gmail.com>
2026-05-08 22:23:32 -0300

    fix 2

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -9,14 +9,8 @@ const getBaseUrl = () => {
 
 export const auth = createNeonAuth({
   baseUrl: getBaseUrl(),
-  trustedOrigins: [
-    'http://localhost:3000',
-    'http://192.168.0.109:3000',
-    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
-    ...(process.env.VERCEL_PROJECT_PRODUCTION_URL ? [`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`] : []),
-    'https://hdb-service.vercel.app'
-  ],
   cookies: {
     secret: process.env.NEON_AUTH_COOKIE_SECRET!,
   },
 });
+
